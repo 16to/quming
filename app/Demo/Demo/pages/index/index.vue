375affa91c56ae2d8d724e27bbcc6b11acf64351
16to <zj@16to.com>
2020-10-24 19:37:08 +0800

    show demo

---
+++ app/Demo/Demo/pages/index/index.vue
@@ -0,0 +1,70 @@
+<template>
+	<view class="content">
+		<view class="uni-list">
+			<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,index) in news" :key="index" @click="openinfo"
+			 :data-newsid="item.post_id">
+				<view class="uni-media-list">
+					<image class="uni-media-list-logo" :src="item.author_avatar"></image>
+					<view class="uni-media-list-body">
+						<view class="uni-media-list-text-top">{{item.title}}</view>
+						<view class="uni-media-list-text-bottom uni-ellipsis">{{item.created_at}}</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Demo',
+				news: [],
+			}
+		},
+		// 当页面加载之后
+		onLoad() {
+			uni.showLoading({
+				title: '加载中',
+			})
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news',
+				method: 'GET',
+				data: {},
+				success: res => {
+					this.news = res.data;
+					uni.hideLoading();
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+		},
+		methods: {
+			openinfo(e) {
+				var newsid = e.currentTarget.dataset.newsid;
+				uni.navigateTo({
+					url: '../info/info?newsid=' + newsid
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+</style>
