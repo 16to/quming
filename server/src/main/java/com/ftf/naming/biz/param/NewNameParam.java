0d92a25baf8026fc9dc71b53541dcff4a6a83833
qumuasa <qumuasa820@163.com>
2020-11-08 12:43:44 +0800

    新华字典随机生成名字

---
+++ server/src/main/java/com/ftf/naming/biz/param/NewNameParam.java
@@ -0,0 +1,10 @@
+package com.ftf.naming.biz.param;
+
+import lombok.Data;
+
+@Data
+public class NewNameParam {
+
+	private String firstName;
+	private String birth;
+}
--- server/src/main/java/com/ftf/naming/biz/vo/NameVO.java
+++ server/src/main/java/com/ftf/naming/biz/vo/NameVO.java
@@ -1,5 +1,7 @@
 package com.ftf.naming.biz.vo;
 
+import com.ftf.naming.biz.dto.NameDTO;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -16,4 +18,18 @@ public class NameVO {
 	private String firstName;
 	@ApiModelProperty("名")
 	private String lastName;
+	@ApiModelProperty("笔画")
+	private String strokes;
+	@ApiModelProperty("拼音")
+	private String pinyin;
+	@ApiModelProperty("解释")
+	private String explanation;
+	
+	public NameVO(String firstName, NameDTO data) {
+		this.firstName = firstName;
+		this.lastName = data.getWord();
+		this.strokes = data.getStrokes();
+		this.pinyin = data.getPinyin();
+		this.explanation = data.getExplanation();
+	}
 }
